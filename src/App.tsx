caff0557c161999fc44be44ae63713a81d71299a
momal <momal.mahar@gmail.com>
2024-10-20 18:35:32 +0200

    added an error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import { useTheme } from './context/ThemeContext';
 import TreeView from './components/Tree/TreeView';
 import './App.css'
 import Toggle from './components/ThemeToggle/Toggle';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App: React.FC = () => {
   const { theme } = useTheme();
 
   return (
-    <Provider store={store}>
-      <div className={`app ${theme}`}>
-        <header>
-          {/* otherwise this would have been in a Header component with proper pages and routing. but we will have to do for now */}
-          <Toggle />
-        </header>
-        <TreeView />
-      </div>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <div className={`app ${theme}`}>
+          <header>
+            {/* otherwise this would have been in a Header component with proper pages and routing. but we will have to do for now */}
+            <Toggle />
+          </header>
+          <TreeView />
+        </div>
+      </Provider>
+    </ErrorBoundary>
   );
 };
 
